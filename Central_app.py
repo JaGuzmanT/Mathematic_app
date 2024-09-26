99a32817d2561f688926cfe754367f7a3397d2a4
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2024-09-26 16:31:34 -0600

    Update Central_app.py

--- Central_app.py
+++ Central_app.py
@@ -14,7 +14,7 @@ st.divider()
 lottie_animation = utils.load_lottiefile("Gifs/Mathematics.json")
 st_lottie(lottie_animation, height=200) # For more information about gifs you can check https://lottiefiles.com/
 
-utils.imagen_inicio()
+# utils.imagen_inicio()
 
 with st.expander(label="Operadores de CalcMaster WebApp", icon="🔥"):
         st.write(":blue[Valor Absoluto] --> :orange[abs()]")
@@ -22,6 +22,3 @@ with st.expander(label="Operadores de CalcMaster WebApp", icon="🔥"):
         st.write(":blue[Multiplicación] --> :orange[*]")
         st.write(":blue[Infinito] --> :orange[oo]")
         st.write(":blue[Raíz cuadrada] --> :orange[sqrt()]")
-
-Author = "Aplicación desarrollada por el Dr. José Alberto Guzmán" 
-st.write(f"<h4 style = 'text-align:center'> {Author} </h4>", unsafe_allow_html=True) 
