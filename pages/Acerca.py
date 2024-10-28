0e2fc0259fa78c1d74186daee34eec786d28c444
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2024-10-28 09:03:43 -0600

    Create Acerca.py

---
+++ pages/Acerca.py
@@ -0,0 +1,10 @@
+# Charging libraries
+import streamlit as st
+import sympy as sp
+import Utilities as utils
+
+# -------------------- Setting the main page of the webapp -------------------- #
+utils.page_configuration()
+utils.hide_elements()
+utils.sidebar_elements()
+
